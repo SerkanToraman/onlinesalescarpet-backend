e2f0f4498753294214efd5a5335adc0119272273
serkantoraman <serkan@github.com>
2023-10-21 14:56:56 +0300

    Add fringe entity, Repo, Service and controller

---
+++ src/main/java/com/onlineCarpetSales/backend/repository/FringeRepository.java
@@ -0,0 +1,8 @@
+package com.onlineCarpetSales.backend.repository;
+
+
+import com.onlineCarpetSales.backend.entity.Fringe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FringeRepository extends JpaRepository<Fringe, Integer> {
+}
